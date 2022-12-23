2b8205178e5640cf19dce341d4a97af99f35b320
p.gerasimov <gerasimov86@gmail.com>
2022-12-23 20:52:03 +0300

    create db and admin

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import logout_user
-
 from webapp.forms import LoginForm, RegistrationForm
 from webapp.model import db, Users
 
