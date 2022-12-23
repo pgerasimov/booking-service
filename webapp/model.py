6adb3a60b13f34f1e3c0ad16bc51d8e4c1813a28
p.gerasimov <gerasimov86@gmail.com>
2022-12-23 20:33:10 +0300

    login and user model

---
+++ webapp/model.py
@@ -0,0 +1,22 @@
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(120))
+    last_login = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
